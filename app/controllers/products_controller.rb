eee74c0fc04fdfa41538c91f56b88e1e19664798
Manoj Menon <mhmenon910@gmail.com>
2015-08-27 11:39:20 +0400

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,9 +70,9 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-    #  params.require(:product).permit(:name, :price)
- params.require(:product).permit(:name,:price,:produt_type_id,:properties).tap do |whitelisted|
-        whitelisted[:properties] = params[:product][:properties]
-      end
+      #params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:name,:price,:produt_type_id,:properties).tap do |whitelisted|
+      whitelisted[:properties] = params[:product][:properties]
+    end
     end
 end
